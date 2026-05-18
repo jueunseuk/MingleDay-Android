ececd7735ea38195efad786cbb1c8ade6dad6794
returns0825 <junsu120202@gmail.com>
2026-05-18 21:13:49 +0900

    feat: search view

---
+++ app/src/main/java/returns/mingleday/app/data/remote/model/user/ScheduleStatus.kt
@@ -0,0 +1,7 @@
+package returns.mingleday.app.data.remote.model.user
+
+enum class ScheduleStatus {
+    TODO,
+    COMPLETED,
+    CANCELED
+}
--- app/src/main/java/returns/mingleday/app/ui/main/search/SearchFragment.kt
+++ app/src/main/java/returns/mingleday/app/ui/main/search/SearchFragment.kt
@@ -1,6 +1,8 @@
 package returns.mingleday.app.ui.main.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,5 +25,33 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         (requireActivity() as MainActivity).setToolbarTitle(R.string.search_title)
+
+        setupValidation()
+        setupSearchRequest()
+        setupRecyclerView()
     }
+
+    private fun setupRecyclerView() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupSearchRequest() {
+
+    }
+
+    private fun setupValidation() {
+        val watcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val isNameLengthValid = binding.inputSearchKeyword.length() in 1..30
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+
+        binding.inputSearchKeyword.addTextChangedListener(watcher)
+    }
+
+
 }
