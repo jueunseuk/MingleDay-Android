f22e04deb60a335eec8db054299b6b4e716f0c07
returns0825 <junsu120202@gmail.com>
2026-05-20 15:26:21 +0900

    feat: add side menu to assign mingle id

--- app/src/main/java/returns/mingleday/app/ui/main/MainActivity.kt
+++ app/src/main/java/returns/mingleday/app/ui/main/MainActivity.kt
@@ -2,24 +2,35 @@ package returns.mingleday.app.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.coroutines.launch
 import returns.mingleday.R
 import returns.mingleday.app.data.remote.intercepter.SessionManager
+import returns.mingleday.app.data.remote.network.onError
+import returns.mingleday.app.data.remote.network.onException
+import returns.mingleday.app.data.remote.network.onSuccess
+import returns.mingleday.app.data.repository.MingleRepository
 import returns.mingleday.app.ui.auth.LoginActivity
 import returns.mingleday.app.ui.main.mingle.MingleListFragment
 import returns.mingleday.app.ui.main.mymenu.MymenuFragment
 import returns.mingleday.app.ui.main.schedule.ScheduleFragment
 import returns.mingleday.app.ui.main.search.SearchFragment
+import returns.mingleday.app.ui.main.side.MingleDrawerAdapter
 import returns.mingleday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val mingleRepository = MingleRepository()
+    private lateinit var mingleDrawerAdapter: MingleDrawerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,8 +50,73 @@ class MainActivity : AppCompatActivity() {
             binding.bottomBar.selectedItemId = R.id.schedule
         }
 
+        setupDrawer()
         setupBottomNavigation()
         setupSession()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_calendar -> {
+                true
+            }
+            R.id.action_alarm -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setupDrawer() {
+        mingleDrawerAdapter = MingleDrawerAdapter { mingle ->
+            binding.drawerLayout.close()
+            val fragment = ScheduleFragment().apply {
+                arguments = Bundle().apply {
+                    putInt("mingleId", mingle.mingleId)
+                }
+            }
+
+            replaceFragment(fragment)
+            binding.bottomBar.selectedItemId = R.id.schedule
+        }
+
+        binding.drawerMingleRecyclerView.layoutManager =
+            LinearLayoutManager(this)
+
+        binding.drawerMingleRecyclerView.adapter = mingleDrawerAdapter
+
+        binding.topBar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+            fetchDrawerMingles()
+        }
+    }
+
+    private fun fetchDrawerMingles() {
+        lifecycleScope.launch {
+            mingleRepository.getMyMingles()
+                .onSuccess { response ->
+                    mingleDrawerAdapter.submitList(response)
+                }
+                .onError {
+                    Toast.makeText(
+                        this@MainActivity,
+                        R.string.internal_server_error,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                .onException {
+                    Toast.makeText(
+                        this@MainActivity,
+                        R.string.internal_server_error,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+        }
     }
 
     fun setToolbarTitle(resId: Int) {
--- app/src/main/java/returns/mingleday/app/ui/main/schedule/ScheduleFragment.kt
+++ app/src/main/java/returns/mingleday/app/ui/main/schedule/ScheduleFragment.kt
@@ -1,6 +1,7 @@
 package returns.mingleday.app.ui.main.schedule
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +15,8 @@ class ScheduleFragment : Fragment() {
     private var _binding: FragmentScheduleBinding? = null
     private val binding get() = _binding!!
 
+    private var mingleId: Int = -1
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentScheduleBinding.inflate(inflater, container, false)
         return binding.root
@@ -23,5 +26,22 @@ class ScheduleFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         (requireActivity() as MainActivity).setToolbarTitle(R.string.my_schedule_title)
+        mingleId = arguments?.getInt("mingleId") ?: -1
+
+        if (mingleId == -1) {
+            fetchMySchedules()
+        } else {
+            fetchSchedules(mingleId)
+        }
+    }
+
+    private fun fetchMySchedules() {
+        // impl
+        Log.d("ScheduleFragment", "내가 속한 밍글들의 스케줄 불러오기 실행")
+    }
+
+    private fun fetchSchedules(mingleId: Int) {
+        // impl
+        Log.d("ScheduleFragment", "$mingleId 밍글의 스케줄 불러오기 실행")
     }
 }
